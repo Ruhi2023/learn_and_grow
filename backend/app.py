1d89d9568cf9b1260ddabd1bea5707870c760c13
ruhi-wq <Ruhi.j@pharmacoevidence.com>
2025-04-09 23:44:56 +0530

    first commit

---
+++ backend/app.py
@@ -0,0 +1,6 @@
+# there will be 7 types of api classes
+# 1. chat apis
+# 2. learning apis
+# 3. user login apis
+# 4. database apis
+# 5. others as per need
---
+++ backend/test.py
@@ -0,0 +1,2 @@
+from langchain_milvus import BM25BuiltInFunction, Milvus
+help(Milvus)
